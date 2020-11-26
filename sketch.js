0307db3bbecab967dd8670b20e4dab1459966d0b
bijoya <bijoya10101@gmail.com>
2020-11-27 01:52:47 +0530

    anvil

--- sketch.js
+++ sketch.js
@@ -4,11 +4,10 @@ const Bodies = Matter.Bodies;
 
 var engine, world;
 var player, back, boxArray = [];
-var flag = 0;
+var flagToReset = "start",flag1=0, flagBox = "start",flag=0;
 var anvilArray = [];
-var ground
+var ground;
 var sc;
-var flag1=0, flag3 = 0;
 
 function preload() {
     back = loadImage("images/minecraftB.jpg");
@@ -20,45 +19,56 @@ function setup(){
     world = engine.world;
 
     player = new Player(200,200);
+    player.isSensor=true
     for (var i=50; i<1000; i = i+100) {
         anvilArray.push(new Anvil(i,50));
+
     }
     ground = new Ground(500,600);
-
+    sc = second();
+    boxCreate();
 }
 
 function draw(){
-    background(0);
     if (back) {
         background(back);
     }
     Engine.update(engine);
-    //console.log(player.body.position.y);
+
+    if(second()%15===0){
+        sc = second();
+    }
 
     move();
 
-    if (flag3 === 0 && second()%10===0) {
-        boxCreate();
-        flag3 = 1;
-    }
 
+    //displaying everuything
     for (var i=0; i<boxArray.length; i++) {
         boxArray[i].display();
     }
     for (var i=0; i<anvilArray.length; i++) {
         anvilArray[i].display();
-        if (boxArray.length === 2) {
-            //console.log(anvilArray[i])
-            Matter.Body.setStatic(anvilArray[i].body,false);
-        }
+        anvilArray[i].isSensor=true
     }
     ground.display();
 
-    if ((sc + 5)%60 === second() && flag3 === 1) {
-        boxReset();
-        console.log("hi");
 
+    //anvil drop && reset
+    if ((sc + 5)%60 === second()&& flagToReset==="start") {
+        anvilDrop();
+        flagToReset="dropped";
+       
     }
+    if(sc+10%60===second() && flagToReset==="dropped"){
+        anvilReset();
+        boxReset();
+        for (var i=0; i<anvilArray.length; i++) {  
+        }
+        flagToReset="start";
+    }
+   
+    
+    console.log(player.isSensor)
 
 }
 
@@ -77,7 +87,6 @@ function move() {
         flag1=2
     } else if(keyIsDown(UP_ARROW)) {
         flag1=3;
-
     }
 
     if(flag1===1){
@@ -89,47 +98,27 @@ function move() {
     }
 } 
 
-function keyPressed() {
-    if (keyCode === UP_ARROW && player.body.position.y >= 480 ) {
-        Matter.Body.applyForce(player.body,player.body.position,{x:100,y:-200});
-    }
-    if (keyCode === DOWN_ARROW && player.body.position.y >= 480 ) {
-        Matter.Body.applyForce(player.body,player.body.position,{x:-100,y:-200});
-    }
-    /*if (keyCode === RIGHT_ARROW) {
-        player.displayRight();
-    }
-    if (keyCode === LEFT_ARROW) {
-        player.displayLeft();
-    }*/
-}
 
 function boxCreate() {
-    sc = second();
-    var rand = Math.round(random(1,4));
-    if (rand === 1) {
-        
-    }
-    if (flag === 0) {
-        boxArray.push(new Block(random(50,450),300));
-        flag = 1;
-    }  else if (flag === 1) {
-        boxArray.push(new Block(random(550,950),300));
-        flag = 2;
-    }     
+    boxArray.push(new Block(random(50,450),300));
+    boxArray.push(new Block(random(550,950),300)); 
+}
+function boxReset(){
+    Matter.Body.setPosition(boxArray[0].body,{x:random(50,450),y:300})
+    Matter.Body.setPosition(boxArray[1].body,{x:random(550,950),y:300})
 }
 
-function boxReset() {
-    boxArray = [];
-    console.log(boxArray);
-    //boxArray[0].body.position.y = 100
-    //boxArray.position.x = 100;
-    console.log("hello");
+function anvilDrop() {
     for (var i = 0; i<anvilArray.length; i++) {
-        anvilArray[i].body.position.y = 50;
-        anvilArray[i].body.position.x = i * 100;
         Matter.Body.setStatic(anvilArray[i].body,false);
     }
 }
-//find out how to delete the boxes
-    
+
+function anvilReset(){
+    for (var i=0; i<anvilArray.length; i++) {
+       Matter.Body.setPosition(anvilArray[i].body,{x:i * 100,y:50})
+       Matter.Body.setStatic(anvilArray[i].body,true);    
+    }  
+}
+
+
